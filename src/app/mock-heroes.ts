206907559a73e6604a4f75252743eb205d5dcfa3
prasad <prasadiyer12345@gmail.com>
2021-02-23 16:41:50 +0530

    initial commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import {Hero} from './hero'
+
+export const HEROES: Hero[] = [
+    {id: 2, name: 'Dr.Doom'},
+    {id: 3, name: 'Hulk'},
+    {id: 4, name: 'catWoman'},
+    {id: 5, name: 'IronMan'},
+    {id: 6, name: 'Captain America'},
+    {id: 7, name: 'Hawk Eye'},
+    {id: 8, name: 'Loki'},
+    {id: 9, name: 'Thor'},
+    {id: 10, name: 'Black Widow'},
+]
